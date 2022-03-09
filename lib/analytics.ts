9888597538f539102c28f57ebe4dea1211fdb83a
Marcelo Oliveira <marcelotk_15@hotmail.com>
2022-03-09 10:52:10 -0300

    🐛 FIX: check env valu is ok

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -7,7 +7,7 @@ export function useAnalytics () {
 
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
-      Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
+      Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID || '', {
         includedDomains: ['teka.dev']
       });
     }
