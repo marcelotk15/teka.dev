391eccfa1ad108ea464f04a3bd9af126dcfc1ed9
Breno Cota <106157862+brenocota-hotmart@users.noreply.github.com>
2022-10-01 20:32:31 -0300

    fix: read all posts link

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home ({
             <RecentPosts posts={posts} />
           )}
           
-          <LinkStyled as={'a'}>
+          <LinkStyled as={'a'} href="/blog">
             Read all posts
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"></path>
