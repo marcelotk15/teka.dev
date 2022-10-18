a4f05a1b0276c1c9d6ead29bddb84b562b72919e
Marcelo Oliveira <marcelotk_15@hotmail.com>
2022-10-18 13:02:04 -0300

    👌 IMPROVE: 🐛 FIX:

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,7 +1,7 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
-import sanityConfig from '@/sanity.config'
+import sanityConfig from 'sanity.config'
 
 export const imageBuilder = createImageUrlBuilder(sanityConfig)
 
