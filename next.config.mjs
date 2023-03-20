0cb5ccd6bcb7a608325b4ce21e241374b58ddd55
marcelotk15 <marcelotk_15@hotmail.com>
2023-03-20 19:24:26 -0300

    📦 NEW: lint

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,9 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env.mjs'))
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-};
-export default config;
+}
