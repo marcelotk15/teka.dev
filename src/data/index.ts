df63e69933c5cd1acabbec16aa448b16ce6d9e4c
marcelotk15 <marcelotk_15@hotmail.com>
2023-03-21 18:44:31 -0300

    👌 IMPROVE: site data

---
+++ src/data/index.ts
@@ -0,0 +1,18 @@
+import { Icons } from '@/components/Icons'
+
+export const navigationLinks = [
+  { to: '/', name: 'Home', Icon: Icons.home },
+  // { to: '/blog', name: 'Blog', Icon: Icons.pencil },
+  // { to: '/guestbook', name: 'Guestbook', Icon: BookBookmark },
+  // { to: '/dashboard', name: 'Dashboard', Icon: IdentificationCard },
+]
+
+export const navigationLinksSocials = [
+  { to: 'https://twitter.com/_marcelotk', name: 'Twitter', Icon: Icons.twitter },
+  { to: 'https://github.com/marcelotk15', name: 'Github', Icon: Icons.github },
+  {
+    to: 'https://linkedin.com/in/marcelo-augusto-s-oliveira/',
+    name: 'LinkedIn',
+    Icon: Icons.linkedin,
+  },
+]
