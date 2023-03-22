4deb122ea4f99196a19990f6ce679d5973deb105
marcelotk15 <marcelotk_15@hotmail.com>
2023-03-22 12:47:50 -0300

    👌 IMPROVE: mobile

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -9,13 +9,13 @@ import { SocialLink } from './SocialLink'
 
 export function Hero() {
   return (
-    <section className="relative -mt-32 overflow-hidden pb-32 pt-[calc(7rem+2.5rem)] after:absolute after:inset-x-0 after:bottom-0 after:h-32 after:bg-gradient-to-b after:from-transparent after:to-zinc-100 dark:after:to-zinc-900 md:pt-[calc(7rem+3rem)]">
+    <section className="relative -mt-32 overflow-hidden pb-16 pt-[calc(7rem+2.5rem)] after:absolute after:inset-x-0 after:bottom-0 after:h-24 after:bg-gradient-to-b after:from-transparent after:to-zinc-100 dark:after:to-zinc-900 md:pb-32 md:pt-[calc(7rem+3rem)] md:after:h-32">
       <div className="absolute inset-0 -z-10 block text-zinc-300 dark:text-zinc-800">
         <TopographyLevels className="h-full md:h-auto md:w-full" />
       </div>
 
       <div className="container">
-        <div className="z-50 font-serif text-3xl">
+        <div className="font-serif text-2xl md:text-3xl">
           <Trans
             i18nKey="home:heroText"
             components={{
