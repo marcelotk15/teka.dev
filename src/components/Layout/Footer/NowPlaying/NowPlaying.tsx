5a1c84be11220c5e747933227cfc24297c986803
marcelotk15 <marcelotk_15@hotmail.com>
2023-03-22 12:47:59 -0300

    👌 IMPROVE: improves

--- src/components/Layout/Footer/NowPlaying/NowPlaying.tsx
+++ src/components/Layout/Footer/NowPlaying/NowPlaying.tsx
@@ -27,7 +27,7 @@ export function NowPlaying() {
         target="_blank"
       >
         <div className="relative my-4 inline-flex items-center gap-5 rounded-lg border border-zinc-400 bg-zinc-200 p-4 dark:border-zinc-700 dark:bg-zinc-800">
-          <div className="absolute inset-0 top-1/2 left-1/2 -z-50 h-[calc(100%+.5rem)] w-[calc(100%+.5rem)] -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-[#ce4de9] via-[#ff6c6a] to-[#f4aa4b] opacity-50 blur-md dark:opacity-30" />
+          <div className="absolute inset-0 top-1/2 left-1/2 -z-10 h-[calc(100%+.5rem)] w-[calc(100%+.5rem)] -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-[#ce4de9] via-[#ff6c6a] to-[#f4aa4b] opacity-50 blur-md dark:opacity-30" />
 
           <div className="text-green-600">{Icons.spotify}</div>
 
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,17 +1,58 @@
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { cva } from 'class-variance-authority'
+
+import { cn } from '@/lib/utils'
 
 import { Logo } from '../../Logo'
 import { ThemeSelector } from './ThemeSelector'
 import { LangSelector } from './LangSelector'
 
+const headerVariants = cva(
+  'sticky inset-x-0 top-0 z-40 flex w-full flex-wrap text-sm sm:flex-nowrap sm:justify-start sm:py-0 transition-all',
+  {
+    variants: {
+      variant: {
+        false: '',
+        true: 'backdrop-blur bg-zinc-900/50',
+      },
+    },
+    defaultVariants: {
+      variant: false,
+    },
+  },
+)
+
+const wrapperVariations = cva('container flex transition-[padding]', {
+  variants: {
+    variant: {
+      false: 'py-6',
+      true: 'py-4',
+    },
+  },
+  defaultVariants: {
+    variant: false,
+  },
+})
+
 export function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+
+    window.addEventListener('scroll', () => {
+      setIsScrolled(window.scrollY > 50)
+    })
+  }, [])
+
   return (
-    <header className="sticky z-40">
-      <div className="container flex py-6">
+    <header className={cn(headerVariants({ variant: isScrolled }))}>
+      <div className={cn(wrapperVariations({ variant: isScrolled }))}>
         <nav className="flex w-full items-center">
           <div>
             <Link aria-label="teka" href="/">
-              <Logo className="h-6 md:h-10" />
+              <Logo className="h-7 md:h-10" />
             </Link>
           </div>
 
