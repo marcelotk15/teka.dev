b8f01f03e4de2a5a836ebf7a41d34ed3906e83f6
marcelotk15 <marcelotk_15@hotmail.com>
2023-03-23 10:17:49 -0300

    👌 IMPROVE: values

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -3,15 +3,29 @@ import type { DefaultSeoProps } from 'next-seo'
 export const SEO: DefaultSeoProps = {
   titleTemplate: '%s | Marcelo "teka" Oliveira',
   defaultTitle: 'Marcelo "teka" Oliveira',
-  description: 'Marcelo "teka" Oliveira.',
+  description: 'Developer',
   canonical: 'https://teka.dev',
   openGraph: {
-    type: 'website',
-    locale: 'id_ID',
+    title: 'Marcelo "teka" Oliveira',
+    description: 'Developer',
     url: 'https://teka.dev',
-    site_name: 'Marcelo "teka" Oliveira',
+    siteName: 'Marcelo "teka" Oliveira',
+    images: [
+      {
+        url: 'https://teka.dev/og.jpg',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
   },
   twitter: {
     cardType: 'summary_large_image',
+  },
+  robotsProps: {
+    maxImagePreview: 'large',
+    maxSnippet: -1,
+    maxVideoPreview: -1,
   },
 }
