00cb5a027be4ac84f0749ce3547db5c7c4998ff8
Marcelo Oliveira <marcelotk_15@hotmail.com>
2023-04-10 21:13:31 -0300

    👌 IMPROVE: button colors

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,18 +4,17 @@ import { ButtonHTMLAttributes, forwardRef } from 'react'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-2 dark:hover:bg-zinc-800 dark:hover:text-zinc-100 disabled:opacity-50 dark:focus:ring-zinc-400 disabled:pointer-events-none dark:focus:ring-offset-zinc-900 data-[state=open]:bg-zinc-100 dark:data-[state=open]:bg-zinc-800',
+  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-zinc-800 hover:text-zinc-100 disabled:opacity-50 focus:ring-zinc-400 disabled:pointer-events-none focus:ring-offset-zinc-900 data-[state=open]:bg-zinc-800',
   {
     variants: {
       variant: {
-        default: 'bg-zinc-900 text-zinc-100 hover:bg-zinc-700 dark:bg-zinc-50 dark:text-zinc-900',
-        destructive: 'bg-red-500 text-zinc-100 hover:bg-red-600 dark:hover:bg-red-600',
-        outline:
-          'bg-transparent border border-zinc-200 hover:bg-zinc-100 dark:border-zinc-700 dark:text-zinc-100',
-        subtle: 'bg-zinc-200 text-zinc-900 hover:bg-zinc-300 dark:bg-zinc-700 dark:text-zinc-100',
+        default: 'hover:bg-zinc-700 bg-zinc-50 text-zinc-900',
+        destructive: 'bg-red-500 text-zinc-100 hover:bg-red-600',
+        outline: 'bg-transparent border hover:bg-zinc-100 border-zinc-700 text-zinc-100',
+        subtle: 'hover:bg-zinc-300 bg-zinc-700 text-zinc-100',
         ghost:
-          'bg-transparent hover:bg-zinc-200 dark:hover:bg-zinc-800 dark:text-zinc-100 dark:hover:text-zinc-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
-        link: 'bg-transparent underline-offset-4 hover:underline text-zinc-900 dark:text-zinc-100 hover:bg-transparent dark:hover:bg-transparent',
+          'bg-transparent hover:bg-zinc-800 text-zinc-100 hover:text-zinc-100  data-[state=open]:bg-transparent',
+        link: 'bg-transparent underline-offset-4 hover:underline text-zinc-100 hover:bg-transparent',
       },
       size: {
         default: 'h-10 py-2 px-4',
